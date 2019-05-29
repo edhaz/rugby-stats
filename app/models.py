0324ffb76886825d44d5c1cda04a1e3eada7e243
Ed Hazledine <ed.hazledine@gmail.com>
2019-05-29 16:51:42 +0100

    Add migrations

--- app/models.py
+++ app/models.py
@@ -1,10 +1,21 @@
+from datetime import datetime
 from app import db
 
 
-class Rugby(db.Model):
+class Teams(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(64), index=True, unique= True)
-    place = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Team: {}>'.format(self.team_name)
+
+
+class Positions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    position = db.Column(db.Integer)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+
+    def __repr__(self):
+        return '<Position {} for team: {}'.format(self.position, self.team_id.team_name)
+
