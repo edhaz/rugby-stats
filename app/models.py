43d67b506992744b32544ea05981bc12eb300f33
Ed Hazledine <ed.hazledine@gmail.com>
2019-05-30 17:55:17 +0100

    Working on psql db

--- app/models.py
+++ app/models.py
@@ -1,21 +1,29 @@
-from datetime import datetime
 from app import db
 
 
 class Teams(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(64), index=True, unique= True)
+    stats = db.relationship('Stats', backref='team', lazy='dynamic')
 
     def __repr__(self):
         return '<Team: {}>'.format(self.team_name)
 
 
-class Positions(db.Model):
+class Stats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    position = db.Column(db.Integer)
+    round = db.Column(db.Integer, nullable=False)
+    place = db.Column(db.Integer, nullable=False)
+    # won = db.Column(db.Integer, nullable=False)
+    # drawn = db.Column(db.Integer, nullable=False)
+    # lost = db.Column(db.Integer, nullable=False)
+    # points_for = db.Column(db.Integer, nullable=False)
+    # points_against = db.Column(db.Integer, nullable=False)
+    # points_difference = db.Column(db.Integer, nullable=False)
+    # bonus = db.Column(db.Integer, nullable=False)
+    # points = db.Column(db.Integer, nullable=False)
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
 
     def __repr__(self):
-        return '<Position {} for team: {}'.format(self.position, self.team_id.team_name)
+        return '<Position {} for team: {}>'.format(self.place, self.team_id.team_name)
 
