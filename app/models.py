18c4e2322d83785902e8ec01b8fa031f58a263fa
Ed Hazledine <ed.hazledine@gmail.com>
2019-05-31 11:27:52 +0100

    Working on db model relationships

--- app/models.py
+++ app/models.py
@@ -1,19 +1,46 @@
 from app import db
 
 
-class Teams(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String(64), index=True, unique= True)
-    stats = db.relationship('Stats', backref='team', lazy='dynamic')
+    username = db.Column(db.String(64), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<Team: {}>'.format(self.team_name)
+        return '<User {}>'.format(self.username)
 
 
-class Stats(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    round_number = db.Column(db.Integer, nullable=False)
-    place = db.Column(db.Integer, nullable=False)
+    round_no = db.Column(db.Integer)
+    place = db.Column(db.Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {} for author: {}>'.format(self.round_no, self.user_id)
+
+
+# class Team(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     team_name = db.Column(db.String(64), index=True, unique= True)
+#     stats = db.relationship('Stat', backref='team', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Team: {}>'.format(self.team_name)
+#
+#
+# class Stat(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     round_number = db.Column(db.Integer, nullable=False)
+#     place = db.Column(db.Integer, nullable=False)
+#     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+#
+#     def __repr__(self):
+#         return '<Position {} for team_id: {}>'.format(self.place, self.team_id)
+
+
+
+
     # won = db.Column(db.Integer, nullable=False)
     # drawn = db.Column(db.Integer, nullable=False)
     # lost = db.Column(db.Integer, nullable=False)
@@ -22,8 +49,4 @@ class Stats(db.Model):
     # points_difference = db.Column(db.Integer, nullable=False)
     # bonus = db.Column(db.Integer, nullable=False)
     # points = db.Column(db.Integer, nullable=False)
-    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
-
-    def __repr__(self):
-        return '<Position {} for team: {}>'.format(self.place, self.team_id.team_name)
 
