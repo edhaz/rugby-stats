4ce7413ca98388fabe18c7e484cc7a450323d041
Ed Hazledine <ed.hazledine@gmail.com>
2020-08-04 08:54:17 +0100

    Add collapisble tables - refactoring

--- update_time_getter.py
+++ update_time_getter.py
@@ -5,7 +5,7 @@ from dateutil import parser
 from os import path, listdir
 
 def update_time_getter(year):
-    PATH = path.join(path.dirname(__file__), 'data_archive', str(year))
+    PATH = path.join(path.dirname(__file__), 'data', str(year))
     FILE_NAMES = sorted([item for item in listdir(PATH)])
     date_list = []
     date_regex = re.compile(r'(\d+)-(\d+)-(\d+)')
